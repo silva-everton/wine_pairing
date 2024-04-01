9831f535f62bc9d60b367efb332af4b630efd3c0
silva-everton <amorimsilva.everton@gmail.com>
2024-04-01 22:56:10 +1100

    food menu page setup

--- app/(app)/foodMenu.js
+++ app/(app)/foodMenu.js
@@ -1,3 +1,3 @@
-import { View, Text, SafeAreaView, StyleSheet, ImageBackground, FlatList, TouchableOpacity, Image, TextInput,KeyboardAvoidingView, Platform } from "react-native";    
+import { View, Text, SafeAreaView, StyleSheet, ImageBackground, FlatList, TouchableOpacity, Image } from "react-native";    
 import { Link } from "expo-router";
 import { useNavigation } from '@react-navigation/native';
