cbd994a7afdf6b94efa983a1c7c7a8373d61954e
silva-everton <amorimsilva.everton@gmail.com>
2024-04-02 16:54:49 +1100

    meat options page set up

--- app/meatOptions.js
+++ app/meatOptions.js
@@ -2,22 +2,26 @@ import { View, Text, SafeAreaView, StyleSheet, ImageBackground, FlatList, Toucha
 import { Link } from "expo-router";
 import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
-export default function FoodMenu() {
-  const [search, setSearch] = useState('');
+export default function meatOptions (){
+    const [search, setSearch] = useState('');
   const navigation = useNavigation();
 
   const menuData = [
-    { id: '1', title: 'Meat', route: 'meatOptions', image: require('../assets/meat.png')},
-    { id: '2', title: 'Seafood', route: 'wineDescription', image: require('../assets/seafood.png')},
-    { id: '3', title: 'Pasta', route: 'wineDescription', image: require('../assets/pasta.png')},
-    { id: '4', title: 'Pizza', route: 'wineDescription', image: require('../assets/pizza.png')},
-    { id: '5', title: 'Ethnic', route: 'wineDescription', image: require('../assets/padthai.png')},
-    { id: '6', title: 'Vegetables', route: 'wineDescription', image: require('../assets/vegetables.png')},
-    { id: '7', title: 'Salads', route: 'wineDescription', image: require('../assets/salad.png')},
-    { id: '8', title: 'Cheese', route: 'wineDescription', image: require('../assets/cheese.png')},
-   
+    { id: '1', title: 'Beef Bourguignon', route: 'wineDescription'},
+    { id: '2', title: 'Beef Burgers', route: 'wineDescription'},
+    { id: '3', title: 'Beef Stews & Ragouts', route: 'wineDescription'},
+    { id: '4', title: 'Meatballs', route: 'wineDescription'},
+    { id: '5', title: 'Breast of Chicken', route: 'wineDescription'},
+    { id: '6', title: 'Fried Chicken', route: 'wineDescription'},
+    { id: '7', title: 'Chicken with Mushrooms', route: 'wineDescription'},
+    { id: '8', title: 'Breast of Duck', route: 'wineDescription'},
+    { id: '9', title: 'Roast Turkey - Citrus Marinated', route: 'wineDescription'},
+    { id: '10', title: 'Pork Stir Fry', route: 'wineDescription'},
+    { id: '11', title: 'Steak Tarture', route: 'wineDescription'},
+    { id: '12', title: 'Sirloin Steak', route: 'wineDescription'},   
            
   ];
 
@@ -26,9 +30,12 @@ export default function FoodMenu() {
 
   const renderItem = ({ item }) => (
     <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate(item.route)}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image source={item.image} style={styles.menuImage} />
-        <Text style={styles.menuText}>{item.title}</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Image source={item.image} style={styles.menuImage} />
+          <Text style={styles.menuText}>{item.title}</Text>
+        </View>
+        <Icon name="chevron-right" size={15} color="#000" style={{ marginRight: 10 }} />
       </View>
     </TouchableOpacity>
   );
@@ -36,8 +43,8 @@ export default function FoodMenu() {
   return (
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
 
-          <ImageBackground source={require('../assets/winery.jpg')} style={styles.image}>
-              <Text style={styles.paragraph}> FOOD CATEGORY </Text>   
+          <ImageBackground source={require('../assets/winery5.png')} style={styles.image}>
+              <Text style={styles.paragraph}> MEAT CATEGORY </Text>   
                   <TextInput
                       style={styles.searchInput}
                       value={search}
@@ -90,23 +97,32 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   item: {
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    backgroundColor: 'rgba(122, 162, 227, 0.4)',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
     borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontWeight: 'bold',
+
   },
   title: {
     fontSize: 16,
   },
   menuItem: {
       backgroundColor: 'white',
-      padding: 15,
-      marginBottom: 10,
-      borderRadius: 5,
+      //padding: 15,
+      //marginBottom: 10,
+      //borderRadius: 5,
       alignSelf: 'stretch', // make the item take up the full width of the container
       marginLeft: 20, // add some margin to the left
       marginRight: 20, // add some margin to the right
+      fontWeight: 'bold',
+      color: 'black',
+      borderTopWidth: 1, // Add this line
+      borderTopColor: '#ddd', // Add this line
   },
     menuText: {
       fontSize: 18,
@@ -132,6 +148,11 @@ const styles = StyleSheet.create({
       marginBottom: 10,
       marginRight: 20,
       marginLeft: 20,
+  },
+  icon: {
+    //marginRight: 10, // add some margin to the right of the icon
+    //marginLeft: 10, // add some margin to the left of the icon
+
   },
 
 
